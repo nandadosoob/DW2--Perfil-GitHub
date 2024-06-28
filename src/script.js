c2c72416f75cc937c9e0635e2968c9710b814d5c
nandadosoob <fer.r.benetti@gmail.com>
2024-06-28 18:13:14 -0300

    omg ta indo

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 const inputUsuario = document.getElementById("nomeUsuario")
 const botaoAdd = document.getElementById("botaoAdiciona")
 const fotoPerfil = document.getElementById("imgPerfil")
+const fotoFundo = document.getElementById("imgFundo")
+const nome = document.getElementById("nomeUsuario")
+const login = document.getElementById("IDlogin")
+const repositorioNome = document.getElementById("nomeRepositorio")
+const descRepositorio = document.getElementById("descricao")
+const linguagens = document.getElementById("ling")
 
 
 botaoAdd.addEventListener("click", () =>{
@@ -14,6 +20,7 @@ botaoAdd.addEventListener("click", () =>{
 function carregaUsuario(){
     let user = "abeazist"
     let url = `https://api.github.com/users/${user}`
+    let nomePerfil = 
 
     fetch(url)
     .then((resposta) => {
@@ -22,6 +29,8 @@ function carregaUsuario(){
     .then((dados) => {
         console.log(dados.avatar_url)
         fotoPerfil.setAttribute("src", dados.avatar_url)
+        console.log(dados.name)
+        nome.setAttribute("h3", dados.name)
     })
     .catch((erro) => console.log(erro))
 }
