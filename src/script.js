9588d2aee669719ed2a38095877e430903bb0a23
nandadosoob <fer.r.benetti@gmail.com>
2024-06-29 11:31:34 -0300

    ai deus

--- src/script.js
+++ src/script.js
@@ -9,30 +9,73 @@ const descRepositorio = document.getElementById("descricao")
 const linguagens = document.getElementById("ling")
 
 
-botaoAdd.addEventListener("click", () =>{
-    if(inputUsuario === ""){
+botaoAdd.addEventListener("click", () => {
+    if (inputUsuario === "") {
 
     }
 })
 
 
 
-function carregaUsuario(){
-    let user = "abeazist"
+function carregaUsuario() {
+    // let user = inputUsuario.value
+    let user = "nandadosoob"
     let url = `https://api.github.com/users/${user}`
-    let nomePerfil = 
 
     fetch(url)
-    .then((resposta) => {
-         return resposta.json()
-    })
-    .then((dados) => {
-        console.log(dados.avatar_url)
-        fotoPerfil.setAttribute("src", dados.avatar_url)
-        console.log(dados.name)
-        nome.setAttribute("h3", dados.name)
-    })
-    .catch((erro) => console.log(erro))
+        .then((resposta) => {
+            return resposta.json()
+        })
+        .then((dados) => {
+            //foto de perfil
+            console.log(dados.avatar_url)
+            fotoPerfil.setAttribute("src", dados.avatar_url)
+
+            //nome
+            console.log(dados.name)
+            nome.innerHTML = dados.name;
+
+            //login
+            console.log(dados.login)
+            login.innerHTML = dados.login;
+
+        })
+
+        .catch((erro) => console.log(erro))
 }
 
+function carregaRespositorio() {
+    let user = "nandadosoob"
+
+    let repository = "Dw1"
+    let repositoryUrl = `https://api.github.com/repos/${user}/${repository}`
+    // let repositoryUrl = `https://api.github.com/users/${user}/repos/${repository}`
+    // let repositoryUrl = `https://api.github.com/${user}/${repository}`
+
+    fetch(repositoryUrl)
+        .then((resposta) => {
+            return resposta.json()
+        })
+        .then ((dadosRepositorio) => {
+
+            //nome do respositorio
+            console.log(dadosRepositorio.name)
+            repositorioNome.innerHTML = dadosRepositorio.name
+
+            //descricao do repositorio
+            console.log(dadosRepositorio.description)
+            descRepositorio.innerHTML = dadosRepositorio.description
+        })
+        .catch((erro) => console.log(erro))
+
+    //repositorios
+
+    //nome do repositorio
+
+
+    //linguagens utilizadas
+}
+
+
 carregaUsuario()
+carregaRespositorio()
