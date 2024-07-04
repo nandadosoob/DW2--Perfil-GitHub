c15343513fccd88279918f01cd8fb45e5c7f916d
nandadosoob <fer.r.benetti@gmail.com>
2024-07-04 09:34:28 -0300

    quase pronto, falta so uns detalhes

--- src/script.js
+++ src/script.js
@@ -10,22 +10,19 @@ const descRepositorio = document.getElementById("descricao")
 const linguagens = document.getElementById("ling")
 const caixaUsuario = document.getElementById("caixa")
 const espacoCartao = document.getElementById("espacoCartoes")
-const caixaRepositorios = document.getElementById("divRepositorios")
+
 
 
 botaoAdd.addEventListener("click", () => {
 
     adicionaCartao()
-    // carregaUsuario()
-    carregaRespositorio()
+  
     inputUsuario.value = ''
 })
 
 function adicionaCartao() {
     const novoCard = document.createElement("div")
-    // novoCard.classList = "border-solid border-2 border-stone-400 h-96 w-80 flex flex-col m-4 rounded-md"
     let user = inputUsuario.value
-    // let user = "nandadosoob"
     let url = `https://api.github.com/users/${user}`
 
     fetch(url)
@@ -39,16 +36,18 @@ function adicionaCartao() {
             <div class="border-solid border-2 border-stone-400 h-96 w-80 flex flex-col m-4 rounded-md" id="caixa">
                 <img id="imgPerfil" src=${fotoPerfil}
                     class="rounded-full  w-20 h-20 border-4 border-white self-center absolute top-40">
-                <img id="imgFundo" src="../teste-perfil-git/o-que-e-paisagem.jpg.png" class=" h-28 w-full self-center">
+                <img id="imgFundo" src="../teste-perfil-git/https___dev-to-uploads.s3.amazonaws.com_uploads_articles_4e0d816kuzyu700pdbjn.jpg" class=" h-28 w-full self-center">
                 <br>
                 <br>
                 <p class="self-center" id="nomeUsuario">${dados.name}</p>
                 <p class="self-center" id="IDlogin">${dados.login}</p>
                 <p class="text-xs font-semibold px-4 py-2">REPOSITÓRIOS</p>
-                <div id="divRepositorios" class="w-auto h-44 flex flex-col justify-center gap-3 overflow-auto"></div>
+                <div id="divRepositorios" class="w-auto h-44 flex flex-col items-center gap-3 overflow-auto"></div>
                 `
                 espacoCartao.appendChild(novoCard)
 
+                const caixaRepositorios = novoCard.querySelector("#divRepositorios");
+                carregaRespositorio(user, caixaRepositorios)
                
             })
 
@@ -58,8 +57,8 @@ function adicionaCartao() {
 }
 
 
-function carregaRespositorio() {
-    let user = inputUsuario.value
+function carregaRespositorio(user,caixaRepositorios) {
+    // let user = inputUsuario.value
 
     let repositoryUrl = `https://api.github.com/users/${user}/repos`
 
@@ -67,15 +66,10 @@ function carregaRespositorio() {
     .then((resposta) => {
         return resposta.json()
     })
-    .then((dadosRepositorio) => {
-        
-        // console.log(dadosRepositorio)
-            const caixaRepositorios = document.getElementById("divRepositorios")
-            
+    .then((dadosRepositorio) => {            
             
             dadosRepositorio.forEach((repo) => {
                 const cartao = document.createElement("div")
-                // cartao.classList = "w-auto h-44 flex flex-col justify-center gap-3 overflow-auto"
                 
                 cartao.innerHTML = `<div class="w-72 h-28 bg-gray-200 rounded-md border-2 border-neutral-300 ps-2 self-center">
                 <h6 class="font-bold text-sm" id="nomeRepositorio">${repo.name}</h6>
@@ -85,6 +79,7 @@ function carregaRespositorio() {
                 </div>`
                 
                 caixaRepositorios.appendChild(cartao)
+                console.log(caixaRepositorios)
                 
             });
             
@@ -92,35 +87,3 @@ function carregaRespositorio() {
         .catch((erro) => console.log(erro))
     }
     
-    
-    
-    // carregaUsuario()
-    // carregaRespositorio()
-    // carregaLinguagens()
-    
-    // function carregaUsuario() {
-    //     let user = inputUsuario.value
-    //     // let user = "nandadosoob"
-    //     let url = `https://api.github.com/users/${user}`
-    
-    //     fetch(url)
-    //         .then((resposta) => {
-    //             return resposta.json()
-    //         })
-    //         .then((dados) => {
-    //             //foto de perfil
-    //             console.log(dados.avatar_url)
-    //             fotoPerfil.setAttribute("src", dados.avatar_url)
-    
-    //             //nome
-    //             nome.innerHTML = dados.name;
-    //             console.log(dados.name)
-    
-    //             //login
-    //             console.log(dados.login)
-    //             login.innerHTML = dados.login;
-    
-    //         })
-    
-    //         .catch((erro) => console.log(erro))
-    // }
